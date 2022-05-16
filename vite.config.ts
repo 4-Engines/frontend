f5075b40e56065611ff7581f0a6eb68675bd550e
Carlos <carlosbonadeo@gmail.com>
2022-05-16 21:40:52 +0100

    PWA test implementation

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const path = require('path')
 
@@ -12,6 +13,7 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
+    VitePWA({})
   ],
   define: { 'process.env': {} },
   resolve: {
