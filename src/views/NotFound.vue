eb218c81ae0a7ee27ba9dbee2e6d18556abcc997
Carlos <carlosbonadeo@gmail.com>
2022-05-20 18:34:47 +0100

    Page not found style

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-    Page not found
+  <div class="text-center mt-2">
+    <img src="404.png">
+    <h1>404</h1>
+    <h2>Página no encontrada</h2>
+    <p class="mb-3">Parece que te fuiste de pista</p>
+    <v-btn color="primary" to="/home">Volver al inicio</v-btn>
+  </div>
 </template>
