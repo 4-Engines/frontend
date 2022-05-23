444d89376ccb215a8dbc7ed834d4ce07e4ca55b6
Carlos <carlosbonadeo@gmail.com>
2022-05-23 23:44:36 +0100

    Add RegistroCliente page

---
+++ src/views/RegistroCliente.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Registro</h2>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from "vue";
+import { createUser } from "@/services/User.service";
+
+const form = reactive({
+  username: "",
+  password: "",
+  lastname: "",
+  name: "",
+  phone: "",
+  email: "",
+});
+
+async function register() {
+  try {
+    const response = await createUser(form);
+  } catch (error) {}
+}
+</script>
