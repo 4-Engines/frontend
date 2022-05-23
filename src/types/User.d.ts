ed0538bc9823b7d0ac2391dae771d468d5bd8dff
Carlos <carlosbonadeo@gmail.com>
2022-05-23 23:45:00 +0100

    Redefine User type

--- src/types/User.d.ts
+++ src/types/User.d.ts
@@ -1,10 +1,38 @@
-interface User {
+import { Rol } from "./Enums"
+
+interface BaseUser {
+    id: string,
     name: string,
     last_name: string,
     mail: string,
-    rol: number
 }
 
+type Cliente = BaseUser & {
+    rol: Rol.Cliente
+}
+
+interface Taller {
+    id: string,
+    nombre: string,
+    direccion: string
+}
+
+type Empleado = BaseUser & {
+    rol: Rol.Empleado,
+    taller: Taller,
+}
+
+type Admin = BaseUser & {
+    rol: Rol.Admin,
+    taller: Taller
+}
+
+type User = Cliente | Empleado | Admin
+
 export {
-    User
+    Rol,
+    User,
+    Cliente,
+    Empleado,
+    Admin
 }
