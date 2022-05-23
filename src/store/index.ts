e7a52fc7218b2efe5b339774f87a12b9b75862b6
Carlos <carlosbonadeo@gmail.com>
2022-05-24 00:10:17 +0100

    Fix Store types

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,22 +3,32 @@ import type { User } from '@/types/User'
 import { Rol } from '@/types/Enums'
 
 type Theme = 'light' | 'dark'
-interface Store {
-    theme: Theme,
-    isLoggedIn: boolean,
-    user: User | null
-}
 
-export const useStore = defineStore<string, Store>('main', {
+export const useStore = defineStore('main', {
     state: () => ({
-        theme: 'light',
+        theme: 'light' as Theme,
         isLoggedIn: false,
-        user: null
+        user: null as User | null
     }),
     getters: {
         isCliente: (state) => state.user?.rol === Rol.Cliente,
         isEmpleado: (state) => state.user?.rol === Rol.Empleado,
-        isAdmin: (state) => state.user?.rol === Rol.Admin
+        isAdmin: (state) => state.user?.rol === Rol.Admin,
+        rolLabel() {
+            if (this.isCliente) {
+                return 'Cliente';
+            }
+
+            if (this.isEmpleado) {
+                return 'Empleado';
+            }
+
+            if (this.isAdmin) {
+                return 'Reponsable del taller';
+            }
+
+            return '';
+        }
     },
     persist: true
 })
