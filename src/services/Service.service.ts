c23e8e630d45d9ced614c52b61a13680b4164fe4
Carlos <carlosbonadeo@gmail.com>
2022-06-08 16:03:33 +0100

    Update service

--- src/services/Service.service.ts
+++ src/services/Service.service.ts
@@ -1,11 +1,15 @@
 import { axios } from '@/plugins/axios';
 
 function newService(data: {
-  id_auto: string;
-  comments: string;
-  amount: number;
+  id_car: string;
+  name: string;
+  spare_parts: string;
+  supplies: string;
+  details: string;
+  price: string;
+  id_user: string;
 }) {
-  return axios.post('/nuevo-servicio', data);
+  return axios.post('/create-service', data);
 }
 
 export { newService };
